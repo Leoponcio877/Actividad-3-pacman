a6c082ca4d696717bc81632e74b6626eb9fedab6
jose-duart <jos_durt_sa@gmail.com>
2026-05-08 11:57:43 -0600

    Fantasmas mas rapidos y mapa cambiado

--- pacman.py
+++ pacman.py
@@ -1,44 +1,50 @@
 from random import choice
-from turtle import (Turtle, setup, hideturtle, tracer, update, listen, onkey, ontimer, done)
+from turtle import *
 from freegames import floor, vector
 
-GHOST_SPEED = 8 
+GHOST_SPEED = 10
 PACMAN_SPEED = 5
 TILE_SIZE = 20
 TIMER_MS = 100
+
 state = {'score': 0}
+
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
+
 aim = vector(PACMAN_SPEED, 0)
 pacman = vector(-40, -80)
+
 ghosts = [
     [vector(-180, 160), vector(GHOST_SPEED, 0)],
     [vector(-180, -160), vector(0, GHOST_SPEED)],
     [vector(100, 160), vector(0, -GHOST_SPEED)],
     [vector(100, -160), vector(-GHOST_SPEED, 0)],
 ]
+
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+    0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0,
+    0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
+
 
 def square(x, y):
     "Draw square using path at (x, y)."
@@ -53,6 +59,7 @@ def square(x, y):
 
     path.end_fill()
 
+
 def offset(point):
     "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
@@ -60,64 +67,153 @@ def offset(point):
     index = int(x + y * 20)
     return index
 
+
 def valid(point):
-     index = offset(point)
-     if 0 <= index < len(titles):
-         return titles[index] != 0
-     return False
+    """Return True if point is valid in tiles."""
+    index = offset(point)
+
+    if tiles[index] == 0:
+        return False
+
+    index = offset(point + 19)
+
+    if tiles[index] == 0:
+        return False
+
+    return point.x % 20 == 0 or point.y % 20 == 0
 
 def world():
+    """Draw world using path."""
+    bgcolor('black')
+    path.color('blue')
 
+    for index in range(len(tiles)):
+        tile = tiles[index]
+
+        if tile > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if tile == 1:
+                path.up()
+                path.goto(x + 10, y + 10)
+                path.dot(2, 'white')
 
 def move():
+    """Move pacman and all ghosts."""
+    writer.undo()
+    writer.write(state['score'])
+
+    clear()
+
+    if valid(pacman + aim):
+        pacman.move(aim)
+
+    index = offset(pacman)
+
+    if tiles[index] == 1:
+        tiles[index] = 2
+        state['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+
+    for point, course in ghosts:
+        if valid(point + course):
+            point.move(course)
+        else:
+            options = [
+                vector(GHOST_SPEED, 0),
+                vector(-GHOST_SPEED, 0),
+                vector(0, GHOST_SPEED),
+                vector(0, -GHOST_SPEED),
+            ]
+            plan = choice(options)
+            course.x = plan.x
+            course.y = plan.y
+
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+
+    update()
+
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            return
+
+    ontimer(move, 100)
+
 
 def smart_aim(ghost_pos):
-      direction = [
-      
-          vector( GHOST_SPEED,           0),
-          vector(-GHOST_SPEED,           0),
-          vector(           0,  GHOST_SPEED),
-          vector(           0, -GHOST_SPEED),
-      ]
-      best_dir = None
-      best_dir = float('inf')
-      for d in directions:
+    directions = [
+        vector(GHOST_SPEED, 0),
+        vector(-GHOST_SPEED, 0),
+        vector(0, GHOST_SPEED),
+        vector(0, -GHOST_SPEED),
+    ]
+
+    best_dir = None
+    best_dist = float('inf')
+
+    for d in directions:
         next_pos = ghost_pos + d
+
         if valid(next_pos):
             dist = abs(next_pos.x - pacman.x) + abs(next_pos.y - pacman.y)
+
             if dist < best_dist:
                 best_dist = dist
-                best_dir  = d
- 
+                best_dir = d
+
     return best_dir
+
+
 def draw():
     path.up()
     path.goto(pacman.x + 10, pacman.y + 10)
     path.dot(20, 'yellow')
+
     ghost_colors = ['red', 'pink', 'cyan', 'orange']
+
     for i, (ghost_pos, _) in enumerate(ghosts):
         path.goto(ghost_pos.x + 10, ghost_pos.y + 10)
         path.dot(20, ghost_colors[i % len(ghost_colors)])
+
     update()
+
+
 def change(x, y):
-      new_aim = vector(x, y)
-      if valid(pacman + new_aim):
-          aim.x = x
-          aim.y = y
+    new_aim = vector(x, y)
+
+    if valid(pacman + new_aim):
+        aim.x = x
+        aim.y = y
+
 
 from turtle import bgcolor
+
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
+
 world()
+
 writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
+
 listen()
-onkey(lambda: change( PACMAN_SPEED,       0), 'Right')
-onkey(lambda: change(-PACMAN_SPEED,            0), 'Left')
-onkey(lambda: change(           0,  PACMAN_SPEED), 'Up')
-onkey(lambda: change(           0, -PACMAN_SPEED), 'Down') 
+
+onkey(lambda: change(PACMAN_SPEED, 0), 'Right')
+onkey(lambda: change(-PACMAN_SPEED, 0), 'Left')
+onkey(lambda: change(0, PACMAN_SPEED), 'Up')
+onkey(lambda: change(0, -PACMAN_SPEED), 'Down')
+
 move()
 done()
- 
